ef1a287486fe3216f26c8dde485ec7271d911358
Harsha-msit <harsha.sid94@msitprogram.net>
2021-08-10 11:35:43 +0530

    isfactor

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -6,4 +6,12 @@
 
 
 def fun_isfactor(f, n):
+	if(f==0 and n==0):
+		return True
+	elif(f==0 and n!=0):
+		return False
+	elif(f!=0 and n==0):
+		return True
+	if(n%f==0):
+		return True
 	return False # replace with your solution
