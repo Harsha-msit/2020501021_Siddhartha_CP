f9943cbda2b60c405858fa1a71cde9d49892c937
Harsha-msit <harsha.sid94@msitprogram.net>
2021-08-13 09:41:51 +0530

    removerowandcolumn

--- removeRowAndCol.py
+++ removeRowAndCol.py
@@ -23,6 +23,24 @@
 
 def removeRowAndCol(L, row, col):
     # Your code goes here...
-    pass
+    l=[]
+    out=[]
+    for i in range(len(L)):
+        if(i!=row):
+            l.append(L[i])
+    for i in range(len(l)):
+        la=[]
+        for j in range(len(l[0])):
+            if(j!=col):
+                la.append(l[i][j])
+        out.append(la)
+    return out
 
 # Write your own test cases.
+L=[[2, 3, 4, 5],[8, 7, 6, 5],[0, 1, 2, 3]]
+K=[[1, 2, 3, 4],[5, 6, 7, 8],[9, 10, 11, 12],[13, 14, 15, 16],[17, 18, 19, 20]]
+# print(removeRowAndCol(K,3,3))
+
+assert(removeRowAndCol(L,1,2) == [[2,3,5],[0,1,3]])
+assert(removeRowAndCol(K,3,3) == [[1, 2, 3],[5, 6, 7],[9, 10, 11],[17, 18, 19]])
+print("pass")
